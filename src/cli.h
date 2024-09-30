691154af0e08c82962265879e3b78de95a3c5dad
Cole Johnson <coledjohn@gmail.com>
2024-09-29 20:26:36 -0600

    working on command args

---
+++ src/cli.h
@@ -0,0 +1,5 @@
+#ifndef INCLUDE_SRC_CLI_H_
+#define INCLUDE_SRC_CLI_H_
+// USAGE: spn [-d] [-e] [text] [key]
+#define NUM_ARGS 3
+#endif // INCLUDE_SRC_CLI_H_
--- src/main.c
+++ src/main.c
@@ -1,14 +1,13 @@
+#include "cli.h"
 #include "spc.h"
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
-  Key key = 32768;
-  Block block = 7;
-  block = encrypt(&block, &key, ROUNDS);
-  printf("New block %d\n", block);
-  block = decrypt(&block, &key, ROUNDS);
-  printf("Old block (hopefully) %d\n", block);
+int main(int argc, char *argv[]) {
+  for (int i = 0; i < argc; i++) {
+    char *arg = argv[i];
+    printf("Arg %d: %s\n", i, arg);
+  }
   return EXIT_SUCCESS;
 }
